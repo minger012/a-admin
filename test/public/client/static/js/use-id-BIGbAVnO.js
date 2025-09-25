f1d4a5605d04f5c1919d310d48ac322e41f30bd2
main123 <main123>
2025-09-25 17:16:42 +0800

    client

---
+++ test/public/client/static/js/use-id-BIGbAVnO.js
@@ -0,0 +1,1 @@
+import{p as e}from"./index-WR3xs2eA.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
---
+++ test/public/client/static/js/use-tab-status-C4J5i1ER.js
@@ -0,0 +1,1 @@
+import{M as s}from"./index-WR3xs2eA.js";const t=Symbol(),a=()=>s(t,null);export{t as T,a as u};
