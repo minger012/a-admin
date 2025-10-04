16ef95b830a24bb71450140e1067fc32bd6f6a01
main123 <main123>
2025-10-04 23:05:30 +0800

    client

---
+++ test/public/client/assets/use-id-CrmrtZtk.js
@@ -0,0 +1,1 @@
+import{y as e}from"./index-C8vOelEV.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
--- test/public/client/assets/use-tab-status-1vFvlbEp.js
+++
@@ -1,1 +0,0 @@
-import{P as s}from"./index-CmAqP3Ju.js";const t=Symbol(),a=()=>s(t,null);export{t as T,a as u};
