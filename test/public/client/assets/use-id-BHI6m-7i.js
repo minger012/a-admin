bc7dd72cf590c1f86c511570b45027b543f3f3c9
main123 <main123>
2025-10-06 13:38:54 +0800

    client

--- test/public/client/assets/use-id-BHI6m-7i.js
+++
@@ -1,1 +0,0 @@
-import{C as e}from"./index-DqBSD0aN.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
---
+++ test/public/client/assets/use-tab-status-CXW6XJJT.js
@@ -0,0 +1,1 @@
+import{Y as s}from"./index-t-Fq5SeY.js";const t=Symbol(),a=()=>s(t,null);export{t as T,a as u};
