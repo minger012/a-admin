aa672b68b35081bad4e3be72a83ce0527a3e4432
main123 <main123>
2025-10-12 14:00:33 +0800

    1

--- test/public/client/assets/use-id-Bxx5gKq5.js
+++
@@ -1,1 +0,0 @@
-import{aO as e}from"./index-GnK8NzzB.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
--- test/public/client/assets/use-tab-status-w2X2kA77.js
+++
@@ -1,1 +0,0 @@
-import{aU as s}from"./index-GnK8NzzB.js";const t=Symbol(),o=()=>s(t,null);export{t as T,o as u};
