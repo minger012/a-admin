ed30eb6d9d9ef78d9f6f4038dc85c96a5236ee1b
main123 <main123>
2025-10-18 15:33:55 +0800

    1

--- test/public/client/assets/use-id-D8Fv76oT.js
+++
@@ -1,1 +0,0 @@
-import{aQ as e}from"./index-B7Qbb85S.js";let r=0;function u(){const n=e(),{name:t="unknown"}=(n==null?void 0:n.type)||{};return`${t}-${++r}`}export{u};
---
+++ test/public/client/assets/use-tab-status-Dj4hn7Vs.js
@@ -0,0 +1,1 @@
+import{bi as s}from"./index-CyuuB1M6.js";const t=Symbol(),a=()=>s(t,null);export{t as T,a as u};
